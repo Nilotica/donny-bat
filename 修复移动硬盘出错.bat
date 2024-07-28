676373ec2bf5b3153c2ece6ad6f21d6347595dbc
Donny <donnymoving@gmail.com>
2024-07-28 19:17:53 +0800

    Add a bat.

---
+++ 修复移动硬盘出错.bat
@@ -0,0 +1,11 @@
+@echo off
+title ÐÞ¸´ÒÆ¶¯Ó²ÅÌ³ö´í
+:: Author donnymoving@gmail.com
+echo=
+echo ÊäÈëÒªÐÞ¸´µÄÒÆ¶¯Ó²ÅÌÅÌ·û(ÀýÈç£ºE:):
+set /p input=
+echo ¿ªÊ¼ÐÞ¸´ÒÆ¶¯Ó²ÅÌ
+chkdsk %input%/f
+echo=
+echo ½áÊøÐÞ¸´
+pause>nul&&exit
