a548eee2d96163a6f83a9baa87d7c7ba8ba3be5d
Donny <donnymoving@gmail.com>
2025-10-10 16:12:09 +0800

    优化压缩批处理脚本

--- 压缩当前文件夹下文件夹和文件.bat
+++ 压缩当前文件夹下文件夹和文件.bat
@@ -9,7 +9,7 @@ echo.
 set "filelist=file_list.txt"
 set "timestamp=%date:~0,4%%date:~5,2%%date:~8,2%_%time:~0,2%%time:~3,2%%time:~6,2%"
 set "timestamp=%timestamp: =0%"
-set "zipname=backup_Homalos_%timestamp%.zip"
+set "zipname=backup_%timestamp%.zip"
 
 :: 检查 WinRAR 是否可用
 set "rar_cmd="
